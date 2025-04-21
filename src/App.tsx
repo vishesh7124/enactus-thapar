c1c7a98cabe41a140659abd610ea252658b05357
vishesh7124 <87852777+vishesh7124@users.noreply.github.com>
2025-04-21 23:26:54 +0530

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import BgWrapper from "./components/BgWrapper"
+
+function App() {
+
+  return (
+    <BgWrapper>
+      <div className="flex h-screen w-full items-center justify-center">
+        <h1 className="text-4xl font-bold text-white">Hello World</h1>
+      </div>
+    </BgWrapper>
+      
+  )
+}
+
+export default App
---
+++ src/components/BgWrapper.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const BgWrapper: React.FC<{
+    children?: React.ReactNode;
+}> = ({children}) => {
+  return (
+    <div className="fixed overflow-y-scroll inset-0 -z-20 h-full w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_50%,#d2a829_100%)] bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:100px_100px]">
+      <div className="   inset-0 -z-10 min-h-screen w-full   bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:100px_100px]">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default BgWrapper;
