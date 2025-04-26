95a418552f21f0c8cf749fe367e68e0db57826ea
vishesh7124 <87852777+vishesh7124@users.noreply.github.com>
2025-04-26 17:15:51 +0530

    added core section to team

--- src/data/TeamData.tsx
+++ src/data/TeamData.tsx
@@ -13,3 +13,95 @@ export const Eb: ProfileType[] = [
 
     },
 ]
+export const Core: ProfileType[] = [
+    {
+        name:"Sahir Khan",
+        position: "President",
+        img:president,
+        bgLayer:layer1,
+        linkedin:"https://www.linkedin.com/in/sahirkhan007/",
+        instagram:"https://www.instagram.com/sahirkhann7?igsh=MXVuY2FyajNqZWZ1dw=="
+
+    },
+    {
+        name:"Sahir Khan",
+        position: "President",
+        img:president,
+        bgLayer:layer1,
+        linkedin:"https://www.linkedin.com/in/sahirkhan007/",
+        instagram:"https://www.instagram.com/sahirkhann7?igsh=MXVuY2FyajNqZWZ1dw=="
+
+    },
+    {
+        name:"Sahir Khan",
+        position: "President",
+        img:president,
+        bgLayer:layer1,
+        linkedin:"https://www.linkedin.com/in/sahirkhan007/",
+        instagram:"https://www.instagram.com/sahirkhann7?igsh=MXVuY2FyajNqZWZ1dw=="
+
+    },
+    {
+        name:"Sahir Khan",
+        position: "President",
+        img:president,
+        bgLayer:layer1,
+        linkedin:"https://www.linkedin.com/in/sahirkhan007/",
+        instagram:"https://www.instagram.com/sahirkhann7?igsh=MXVuY2FyajNqZWZ1dw=="
+
+    },
+    {
+        name:"Sahir Khan",
+        position: "President",
+        img:president,
+        bgLayer:layer1,
+        linkedin:"https://www.linkedin.com/in/sahirkhan007/",
+        instagram:"https://www.instagram.com/sahirkhann7?igsh=MXVuY2FyajNqZWZ1dw=="
+
+    },
+    {
+        name:"Sahir Khan",
+        position: "President",
+        img:president,
+        bgLayer:layer1,
+        linkedin:"https://www.linkedin.com/in/sahirkhan007/",
+        instagram:"https://www.instagram.com/sahirkhann7?igsh=MXVuY2FyajNqZWZ1dw=="
+
+    },
+    {
+        name:"Sahir Khan",
+        position: "President",
+        img:president,
+        bgLayer:layer1,
+        linkedin:"https://www.linkedin.com/in/sahirkhan007/",
+        instagram:"https://www.instagram.com/sahirkhann7?igsh=MXVuY2FyajNqZWZ1dw=="
+
+    },
+    {
+        name:"Sahir Khan",
+        position: "President",
+        img:president,
+        bgLayer:layer1,
+        linkedin:"https://www.linkedin.com/in/sahirkhan007/",
+        instagram:"https://www.instagram.com/sahirkhann7?igsh=MXVuY2FyajNqZWZ1dw=="
+
+    },
+    {
+        name:"Sahir Khan",
+        position: "President",
+        img:president,
+        bgLayer:layer1,
+        linkedin:"https://www.linkedin.com/in/sahirkhan007/",
+        instagram:"https://www.instagram.com/sahirkhann7?igsh=MXVuY2FyajNqZWZ1dw=="
+
+    },
+    {
+        name:"Sahir Khan",
+        position: "President",
+        img:president,
+        bgLayer:layer1,
+        linkedin:"https://www.linkedin.com/in/sahirkhan007/",
+        instagram:"https://www.instagram.com/sahirkhann7?igsh=MXVuY2FyajNqZWZ1dw=="
+
+    },
+]
