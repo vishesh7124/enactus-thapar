1e2503164b0dd81488b403d9a129ce8e77ba6507
Aayush Bindal <176171584+Aayush-Bindal@users.noreply.github.com>
2025-08-18 15:10:47 +0530

    Fix for better responsiveness

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -9,7 +9,7 @@ const Banner: React.FC<BannerProps> = ({ title, children }) => {
   return (
     <div className="max-w-[90%]  sm:max-w-7xl sm:w-[1380px] min-h-[445px] h-auto bg-[#363636]/50 rounded-[50px] flex flex-col justify-center py-4 px-8">
       <h1 className="text-[#FFD400] text-4xl sm:text-5xl mb-12">{title}</h1>    
-      <div className="grid grid-flow-col grid-rows-1 sm:grid-rows-2 gap-12 sm:text-2xl ml-4">
+      <div className="grid grid-flow-row grid-rows-1 gap-6 ml-4 lg:grid-flow-col lg:grid-rows-2 lg:gap-12 lg:text-2xl">
         {children}
       </div>
     </div>
